681027e0b830c2a097456591f96aa2f5ef627a54
Joseph Wimalasuriya <67782010+josephrw12@users.noreply.github.com>
2026-05-24 04:47:22 -0700

    Update config.py

--- agent/orchestration/config.py
+++ agent/orchestration/config.py
@@ -1,6 +1,6 @@
 """Configuration for the Executor Client"""
 # Enter the Data Base server and Port here
-SERVER_HOST = "127.0.0.1"
+SERVER_HOST = "127.0.0.1" 
 SERVER_PORT = 9000
 RECV_SIZE = 4096
 
@@ -9,7 +9,7 @@ C_BINARY_RUN_SYSTEM_COMMAND = "../plugins/C/run_system_command"
 PERSIST_BINARY = "../plugins/C/persist_on_startup"
 PRIV_ESC_BINARY = "../plugins/C/priv_esc_CVE-2026-43284"
 
-# On-demand Plugins
+# On-demand Plugins - Team Server URL and PORT
 ON_DEMAND_DIR = "../on_demand_plugins"
 DOWNLOAD_BASE_URL = "http://127.0.0.1:5000/download/"   # <-- Change this
 
